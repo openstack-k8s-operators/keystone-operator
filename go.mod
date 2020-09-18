d19f5e2e9c8bf1927d45a417db76e65ea18fd1ef
James Slagle <jslagle@redhat.com>
2020-09-18 07:56:35 -0400

    Add handling for openstack-config ConfigMap
    The KeystoneApi resource now creates an openstack-config ConfigMap and
    an openstack-config-secret Secret that can be used to configure either
    openstackclient or gopherclient (for talking to Keystone from other
    operators).
    
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/openshift/api v0.0.0-20200205133042-34f0ec8dab87
 	github.com/openstack-k8s-operators/lib-common v0.0.0-20200910130010-129482aabaf9
 	github.com/operator-framework/operator-lifecycle-manager v0.0.0-20200321030439-57b580e57e88
+	gopkg.in/yaml.v2 v2.3.0
 	k8s.io/api v0.18.6
 	k8s.io/apimachinery v0.18.6
 	k8s.io/client-go v0.18.6
---
+++ pkg/keystone/cloudconfig.go
@@ -0,0 +1,44 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package keystone
+
+// OpenStackConfig type
+type OpenStackConfig struct {
+	Clouds struct {
+		Default struct {
+			Auth struct {
+				AuthURL           string `yaml:"auth_url"`
+				ProjectName       string `yaml:"project_name"`
+				UserName          string `yaml:"username"`
+				UserDomainName    string `yaml:"user_domain_name"`
+				ProjectDomainName string `yaml:"project_domain_name"`
+			} `yaml:"auth"`
+			RegionName string `yaml:"region_name"`
+		} `yaml:"default"`
+	}
+}
+
+// OpenStackConfigSecret type
+type OpenStackConfigSecret struct {
+	Clouds struct {
+		Default struct {
+			Auth struct {
+				Password string `yaml:"password"`
+			}
+		}
+	}
+}
