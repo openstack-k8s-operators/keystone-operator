4c6b8263a98abdebbff55d6f6d53dbc8abc10e7b
Dan Prince <dprince@redhat.com>
2022-04-13 10:41:47 -0400

    Forklift controllers/keystoneservice_controller.go from CNOSP

--- controllers/keystoneservice_controller.go
+++ controllers/keystoneservice_controller.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2022.
+
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,45 +18,361 @@ package controllers
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"time"
 
+	"github.com/go-logr/logr"
+	gophercloud "github.com/gophercloud/gophercloud"
+	openstack "github.com/gophercloud/gophercloud/openstack"
+	endpoints "github.com/gophercloud/gophercloud/openstack/identity/v3/endpoints"
+	projects "github.com/gophercloud/gophercloud/openstack/identity/v3/projects"
+	roles "github.com/gophercloud/gophercloud/openstack/identity/v3/roles"
+	services "github.com/gophercloud/gophercloud/openstack/identity/v3/services"
+	users "github.com/gophercloud/gophercloud/openstack/identity/v3/users"
+	keystonev1beta1 "github.com/openstack-k8s-operators/keystone-operator/api/v1beta1"
+	keystone "github.com/openstack-k8s-operators/keystone-operator/pkg/keystone"
+	"gopkg.in/yaml.v2"
+	corev1 "k8s.io/api/core/v1"
+	k8s_errors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	keystonev1beta1 "github.com/openstack-k8s-operators/keystone-operator/api/v1beta1"
 )
 
 // KeystoneServiceReconciler reconciles a KeystoneService object
 type KeystoneServiceReconciler struct {
 	client.Client
-	Scheme *runtime.Scheme
+	Kclient kubernetes.Interface
+	Log     logr.Logger
+	Scheme  *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=keystone.openstack.org,resources=keystoneservices,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=keystone.openstack.org,resources=keystoneservices/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=keystone.openstack.org,resources=keystoneservices/finalizers,verbs=update
-
+// Reconcile keystone service requests
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the KeystoneService object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
 // For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.0/pkg/reconcile
-func (r *KeystoneServiceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+// +kubebuilder:rbac:groups=keystone.openstack.org,resources=keystoneservices,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=keystone.openstack.org,resources=keystoneservices/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=keystone.openstack.org,resources=keystoneservices/finalizers,verbs=update
 
-	// TODO(user): your logic here
+func (r *KeystoneServiceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = r.Log.WithValues("keystoneservice", req.NamespacedName)
+	keystoneAPI := keystone.API(req.Namespace, "keystone")
+	objectKey := client.ObjectKeyFromObject(keystoneAPI)
+	err := r.Client.Get(context.TODO(), objectKey, keystoneAPI)
+	if err != nil {
+		if k8s_errors.IsNotFound(err) {
+			// No KeystoneAPI instance running, return error
+			r.Log.Error(err, "KeystoneAPI instance not found")
+			return ctrl.Result{}, err
+		}
+		// Error reading the object - requeue the request.
+		return ctrl.Result{}, err
+	}
+
+	if keystoneAPI.Status.BootstrapHash == "" {
+		r.Log.Info("KeystoneAPI bootstrap not complete.", "BootstrapHash", keystoneAPI.Status.BootstrapHash)
+		return ctrl.Result{RequeueAfter: time.Second * 5}, err
+	}
+	r.Log.Info("KeystoneAPI bootstrap complete.", "BootstrapHash", keystoneAPI.Status.BootstrapHash)
+
+	// Fetch the KeystoneService instance
+	instance := &keystonev1beta1.KeystoneService{}
+	err = r.Client.Get(context.TODO(), req.NamespacedName, instance)
+	if err != nil {
+		if k8s_errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return ctrl.Result{}, err
+	}
+
+	openStackConfigMap := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "openstack-config",
+			Namespace: instance.Namespace,
+		},
+	}
+	err = r.Client.Get(context.TODO(), types.NamespacedName{Name: openStackConfigMap.Name, Namespace: instance.Namespace}, openStackConfigMap)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	oscm := keystone.OpenStackConfig{}
+	err = yaml.Unmarshal([]byte(openStackConfigMap.Data["clouds.yaml"]), &oscm)
+	r.Log.Info("openStackConfigMap binary data", "binary", openStackConfigMap.BinaryData)
+	r.Log.Info("openStackConfigMap data", "data", openStackConfigMap.Data)
+	r.Log.Info("oscm", "oscm", oscm)
+
+	openStackConfigSecret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "openstack-config-secret",
+			Namespace: instance.Namespace,
+		},
+	}
+	err = r.Client.Get(
+		context.TODO(),
+		types.NamespacedName{
+			Name:      openStackConfigSecret.Name,
+			Namespace: instance.Namespace},
+		openStackConfigSecret)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	oscmSecret := keystone.OpenStackConfigSecret{}
+	sec, err := r.Kclient.CoreV1().Secrets(instance.Namespace).Get(context.TODO(), openStackConfigSecret.Name, metav1.GetOptions{})
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	r.Log.Info("sec", "sec", fmt.Sprintf("%s", sec.Data["secure.yaml"]))
+
+	err = yaml.Unmarshal([]byte(fmt.Sprintf("%s", sec.Data["secure.yaml"])), &oscmSecret)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	r.Log.Info("oscmSecret", "secret", oscmSecret)
+
+	opts := gophercloud.AuthOptions{
+		IdentityEndpoint: oscm.Clouds.Default.Auth.AuthURL,
+		Username:         oscm.Clouds.Default.Auth.UserName,
+		Password:         oscmSecret.Clouds.Default.Auth.Password,
+		TenantName:       oscm.Clouds.Default.Auth.ProjectName,
+		DomainName:       oscm.Clouds.Default.Auth.UserDomainName,
+	}
+
+	provider, err := openstack.AuthenticatedClient(opts)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	endpointOpts := gophercloud.EndpointOpts{Type: "identity", Region: instance.Spec.Region}
+	identityClient, err := openstack.NewIdentityV3(provider, endpointOpts)
+
+	// Create new service if ServiceID is not already set
+	if instance.Status.ServiceID == "" {
+		createOpts := services.CreateOpts{
+			Type:    instance.Spec.ServiceType,
+			Enabled: &instance.Spec.Enabled,
+			Extra: map[string]interface{}{
+				"name":        instance.Spec.ServiceName,
+				"description": instance.Spec.ServiceDescription,
+			},
+		}
+
+		service, err := services.Create(identityClient, createOpts).Extract()
+		if err != nil {
+			r.Log.Error(err, "error")
+			return ctrl.Result{}, err
+		}
+
+		// Set ServiceID in the status
+		r.Log.Info("instance.Status.ServiceID", "ServiceID", instance.Status.ServiceID)
+		r.Log.Info("service.ID", "service.ID", service.ID)
+		if instance.Status.ServiceID != service.ID {
+			instance.Status.ServiceID = service.ID
+			if err := r.Client.Status().Update(context.TODO(), instance); err != nil {
+				r.Log.Error(err, "error")
+				return ctrl.Result{}, err
+			}
+		}
+	} else {
+		// ServiceID is already set, update the service
+		updateOpts := services.UpdateOpts{
+			Type:    instance.Spec.ServiceType,
+			Enabled: &instance.Spec.Enabled,
+			Extra: map[string]interface{}{
+				"name":        instance.Spec.ServiceName,
+				"description": instance.Spec.ServiceDescription,
+			},
+		}
+		_, err := services.Update(identityClient, instance.Status.ServiceID, updateOpts).Extract()
+		if err != nil {
+			r.Log.Error(err, "error")
+			return ctrl.Result{}, err
+		}
+	}
+
+	serviceID := instance.Status.ServiceID
+	reconcileEndpoint(identityClient, serviceID, instance.Spec.ServiceName, instance.Spec.Region, "admin", instance.Spec.AdminURL)
+	reconcileEndpoint(identityClient, serviceID, instance.Spec.ServiceName, instance.Spec.Region, "internal", instance.Spec.InternalURL)
+	reconcileEndpoint(identityClient, serviceID, instance.Spec.ServiceName, instance.Spec.Region, "public", instance.Spec.PublicURL)
+
+	var username string
+	if instance.Spec.Username == "" {
+		username = instance.Spec.ServiceName
+	} else {
+		username = instance.Spec.Username
+	}
+
+	var password string
+	if instance.Spec.Password == "" {
+		// TODO (slagle) use our default password for now until we have
+		// generation.
+		password = "foobar123"
+	} else {
+		password = instance.Spec.Password
+	}
+
+	err = reconcileUser(r.Log, identityClient, username, password)
+	if err != nil {
+		r.Log.Error(err, "error")
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
 
-// SetupWithManager sets up the controller with the Manager.
+// SetupWithManager x
 func (r *KeystoneServiceReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&keystonev1beta1.KeystoneService{}).
 		Complete(r)
+}
+
+func reconcileEndpoint(client *gophercloud.ServiceClient, serviceID string, serviceName string, region string, endpointInterface string, url string) error {
+	// Return if url is empty, likely wasn't specified in the request
+	if url == "" {
+		return nil
+	}
+
+	var availability gophercloud.Availability
+	if endpointInterface == "admin" {
+		availability = gophercloud.AvailabilityAdmin
+	} else if endpointInterface == "internal" {
+		availability = gophercloud.AvailabilityInternal
+	} else if endpointInterface == "public" {
+		availability = gophercloud.AvailabilityPublic
+	} else {
+		return fmt.Errorf("Endpoint interface %s not known", endpointInterface)
+	}
+
+	// Fetch existing endpoint and check it's value if it exists
+	listOpts := endpoints.ListOpts{
+		ServiceID:    serviceID,
+		Availability: availability,
+		RegionID:     region,
+	}
+	allPages, err := endpoints.List(client, listOpts).AllPages()
+	if err != nil {
+		return err
+	}
+	allEndpoints, err := endpoints.ExtractEndpoints(allPages)
+	if err != nil {
+		return err
+	}
+	if len(allEndpoints) == 1 {
+		endpoint := allEndpoints[0]
+		if url != endpoint.URL {
+			// Update the endpoint
+			updateOpts := endpoints.UpdateOpts{
+				Availability: availability,
+				Name:         serviceName,
+				Region:       region,
+				ServiceID:    serviceID,
+				URL:          url,
+			}
+			_, err := endpoints.Update(client, endpoint.ID, updateOpts).Extract()
+			if err != nil {
+				return err
+			}
+		}
+	} else {
+		// Create the endpoint
+		createOpts := endpoints.CreateOpts{
+			Availability: availability,
+			Name:         serviceName,
+			Region:       region,
+			ServiceID:    serviceID,
+			URL:          url,
+		}
+		_, err := endpoints.Create(client, createOpts).Extract()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+
+}
+
+func reconcileUser(reqLogger logr.Logger, client *gophercloud.ServiceClient, username string, password string) error {
+	reqLogger.Info("Reconciling User.", "Username", username)
+
+	var serviceProjectID string
+	allPages, err := projects.List(client, projects.ListOpts{Name: "service"}).AllPages()
+	if err != nil {
+		return err
+	}
+	allProjects, err := projects.ExtractProjects(allPages)
+	if err != nil {
+		return err
+	}
+	if len(allProjects) == 1 {
+		serviceProjectID = allProjects[0].ID
+	} else if len(allProjects) == 0 {
+		createOpts := projects.CreateOpts{
+			Name:        "service",
+			Description: "service",
+		}
+		reqLogger.Info("Creating service project")
+		project, err := projects.Create(client, createOpts).Extract()
+		if err != nil {
+			return err
+		}
+		serviceProjectID = project.ID
+	} else {
+		return errors.New("Multiple projects named \"service\" found")
+	}
+
+	allPages, err = users.List(client, users.ListOpts{Name: username}).AllPages()
+	if err != nil {
+		return err
+	}
+	allUsers, err := users.ExtractUsers(allPages)
+	if err != nil {
+		return err
+	}
+	var userID string
+	if len(allUsers) == 1 {
+		userID = allUsers[0].ID
+	} else {
+		createOpts := users.CreateOpts{
+			Name:             username,
+			DefaultProjectID: serviceProjectID,
+			Password:         password,
+		}
+		user, err := users.Create(client, createOpts).Extract()
+		if err != nil {
+			return err
+		}
+		reqLogger.Info("User Created", "Username", user.Name, "User ID", user.ID)
+		userID = user.ID
+	}
+
+	var adminRoleID string
+	allPages, err = roles.List(client, roles.ListOpts{
+		Name: "admin"}).AllPages()
+	allRoles, err := roles.ExtractRoles(allPages)
+	if len(allRoles) == 1 {
+		adminRoleID = allRoles[0].ID
+	} else {
+		return errors.New("Could not lookup admin role ID")
+	}
+
+	err = roles.Assign(client, adminRoleID, roles.AssignOpts{
+		UserID:    userID,
+		ProjectID: serviceProjectID}).ExtractErr()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -38,6 +38,7 @@ require (
 	github.com/google/gofuzz v1.1.0 // indirect
 	github.com/google/uuid v1.1.2 // indirect
 	github.com/googleapis/gnostic v0.5.5 // indirect
+	github.com/gophercloud/gophercloud v0.24.0 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
@@ -53,8 +54,8 @@ require (
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.19.1 // indirect
-	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/net v0.0.0-20210825183410-e898025ed96a // indirect
+	golang.org/x/crypto v0.0.0-20211202192323-5770296d904e // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
 	golang.org/x/sys v0.0.0-20211029165221-6e7872819dc8 // indirect
 	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
