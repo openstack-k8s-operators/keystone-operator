5376d1582d34932d6935f7cded53f8fa53314242
Dan Prince <dprince@redhat.com>
2022-04-13 08:13:09 -0400

    Clean room

--- templates/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-kolla_set_configs && keystone-manage --config-file=/etc/keystone/keystone.conf \
-bootstrap --bootstrap-password $AdminPassword \
---bootstrap-service-name keystone \
---bootstrap-project-name admin \
---bootstrap-role-name admin \
---bootstrap-admin-url {{.APIEndpoint}} \
---bootstrap-public-url {{.APIEndpoint}} \
---bootstrap-internal-url http://{{.ServiceName}}:5000/  \
---bootstrap-region-id regionOne
