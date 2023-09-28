ebfc090b1dd8ea7f3fba7945c56c1915a9427a61
Balazs Gibizer <gibi@redhat.com>
2023-09-28 11:45:06 +0200

    [envtest]Use database helpers from mariadb-operator
    This is necessary to remove a dependency cycle from lib-common

--- tests/functional/suite_test.go
+++ tests/functional/suite_test.go
@@ -32,6 +32,7 @@ import (
 	keystone_test "github.com/openstack-k8s-operators/keystone-operator/api/test/helpers"
 	"github.com/openstack-k8s-operators/keystone-operator/controllers"
 	common_test "github.com/openstack-k8s-operators/lib-common/modules/test/helpers"
+	mariadb_test "github.com/openstack-k8s-operators/mariadb-operator/api/test/helpers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -47,6 +48,7 @@ var (
 	logger    logr.Logger
 	th        *common_test.TestHelper
 	keystone  *keystone_test.TestHelper
+	mariadb   *mariadb_test.TestHelper
 	namespace string
 )
 
@@ -116,7 +118,9 @@ var _ = BeforeSuite(func() {
 	th = common_test.NewTestHelper(ctx, k8sClient, timeout, interval, logger)
 	Expect(th).NotTo(BeNil())
 	keystone = keystone_test.NewTestHelper(ctx, k8sClient, timeout, interval, logger)
-	Expect(th).NotTo(BeNil())
+	Expect(keystone).NotTo(BeNil())
+	mariadb = mariadb_test.NewTestHelper(ctx, k8sClient, timeout, interval, logger)
+	Expect(mariadb).NotTo(BeNil())
 
 	// Start the controller-manager if goroutine
 	webhookInstallOptions := &testEnv.WebhookInstallOptions
