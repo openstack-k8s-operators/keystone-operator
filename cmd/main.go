99ecaf09fca6d0f5b4cbc5282cec7fd7c6b6579a
Martin Schuppert <mschuppert@redhat.com>
2025-11-25 09:05:54 +0100

    Add pprof-bind-address flag support
    Add optional pprof profiling endpoint configuration via
    --pprof-bind-address flag, matching neutron-operator implementation.
    Disabled by default (empty string).
    
    Signed-off-by: Martin Schuppert <mschuppert@redhat.com>

--- cmd/main.go
+++ cmd/main.go
@@ -78,6 +78,7 @@ func main() {
 	var webhookCertPath, webhookCertName, webhookCertKey string
 	var enableLeaderElection bool
 	var probeAddr string
+	var pprofBindAddress string
 	var secureMetrics bool
 	var enableHTTP2 bool
 	var tlsOpts []func(*tls.Config)
@@ -96,6 +97,7 @@ func main() {
 		"The directory that contains the metrics server certificate.")
 	flag.StringVar(&metricsCertName, "metrics-cert-name", "tls.crt", "The name of the metrics server certificate file.")
 	flag.StringVar(&metricsCertKey, "metrics-cert-key", "tls.key", "The name of the metrics server key file.")
+	flag.StringVar(&pprofBindAddress, "pprof-bind-address", "", "The address the pprof endpoint binds to. Set to empty to disable pprof.")
 	flag.BoolVar(&enableHTTP2, "enable-http2", false,
 		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
 	opts := zap.Options{
@@ -211,6 +213,7 @@ func main() {
 		Metrics:                metricsServerOptions,
 		WebhookServer:          webhookServer,
 		HealthProbeBindAddress: probeAddr,
+		PprofBindAddress:       pprofBindAddress,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "6012128b.openstack.org",
 	}
